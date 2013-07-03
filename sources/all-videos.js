ad6a91ca4ab645a8c93cd7054abcf65fd4e974ca
Mat Janson Blanchet <mat.jb@jansensan.net>
2013-07-02 23:39:42 -0400

    added scenario
    - added scenario image to the read me file
    - added selection of scenes in the js
    - added selection of scenes in max patch

--- sources/all-videos.js
+++ sources/all-videos.js
@@ -2,7 +2,21 @@
 //  [_VAR]  /  Variables
 //=========/------------------------------------------------
 
+// see all scenes at https://rawgithub.com/jansensan/appalaches-projections-soleicare/master/readme.html
+
 var allVideoIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+var fadeInIntro = [1, 2, 3];
+var titleIntro = [6, 4, 5];
+var activeIntro = [4, 5, 8, 22, 21];
+var chorus01 = [11, 10, 9, 12, 6];
+var verse01 = [7, 8, 5, 16, 19];
+var chorus02 = [12, 21, 16, 22, 11, 8, 6];
+var verse02 = [19, 8, 9, 17, 13, 14, 15];
+var chorus03 = [18, 24, 26, 27, 25];
+var break01 = [23];
+var break02 = [29];
+var finale = [30];
 
 var array = allVideoIds;
 var index = -1;
@@ -61,8 +75,63 @@ function list()
 
 function selectArray(id)
 {
-   switch(id)
+	switch(id)
 	{
+		case 1:
+			array = fadeInIntro;
+			post("selected fade in intro videos\n");
+			break;
+		
+		case 2:
+			array = titleIntro;
+			post("selected title intro videos\n");
+			break;
+		
+		case 3:
+			array = activeIntro;
+			post("selected active intro videos\n");
+			break;
+		
+		case 4:
+			array = chorus01;
+			post("selected chorus 01 videos\n");
+			break;
+		
+		case 5:
+			array = verse01;
+			post("selected verse 01 videos\n");
+			break;
+		
+		case 6:
+			array = chorus02;
+			post("selected chorus 02 videos\n");
+			break;
+		
+		case 7:
+			array = verse02;
+			post("selected verse 02 videos\n");
+			break;
+		
+		case 8:
+			array = chorus03;
+			post("selected chorus 03 videos\n");
+			break;
+		
+		case 9:
+			array = break01;
+			post("selected break 01 video\n");
+			break;
+		
+		case 10:
+			array = break02;
+			post("selected break 02 video\n");
+			break;
+		
+		case 11:
+			array = finale;
+			post("selected finale video\n");
+			break;
+
 		default:
 			array = allVideoIds;
 			post("selected all videos\n");
